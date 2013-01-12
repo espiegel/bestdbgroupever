0733cb412ec03393f09c6ce21d2e104ecbe2b546
eidanspiegel@gmail.com <eidanspiegel@gmail.com@0db8492b-292b-fea8-5af4-ef6f3a9017e6>
2013-01-12 16:13:30 +0000

    Shows the rest of the TV Details

--- src/gui/MainDisplay.java
+++ src/gui/MainDisplay.java
@@ -1,13 +1,25 @@
 package gui;
 
+
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.imageio.ImageIO;
 
 import main.Main;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+<<<<<<< .mine
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+=======
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
@@ -15,6 +27,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+>>>>>>> .r37
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
@@ -111,6 +124,36 @@ public class MainDisplay {
 		});
 		mntmLogout.setText("Logout");
 		
+		Group grpDetails = new Group(shlTvTraveler, SWT.NONE);
+		FormData fd_grpDetails = new FormData();
+		fd_grpDetails.bottom = new FormAttachment(0, 230);
+		fd_grpDetails.right = new FormAttachment(0, 1122);
+		fd_grpDetails.top = new FormAttachment(0);
+		fd_grpDetails.left = new FormAttachment(0, 576);
+		grpDetails.setLayoutData(fd_grpDetails);
+		grpDetails.setText("Details");
+		
+		final Label lblDetails1 = new Label(grpDetails, SWT.BORDER);
+		lblDetails1.setBounds(235, 20, 285, 25);
+		
+		final Label lblDetails2 = new Label(grpDetails, SWT.BORDER);
+		lblDetails2.setBounds(235, 57, 285, 25);
+		
+		final Label lblDetails3 = new Label(grpDetails, SWT.BORDER);
+		lblDetails3.setBounds(235, 92, 285, 25);
+		
+		final Label lblDetails4 = new Label(grpDetails, SWT.BORDER);
+		lblDetails4.setBounds(235, 127, 285, 25);
+		
+		final Label lblDetails5 = new Label(grpDetails, SWT.BORDER);
+		lblDetails5.setBounds(235, 162, 285, 25);
+		
+		final Label lblDetails6 = new Label(grpDetails, SWT.BORDER);
+		lblDetails6.setBounds(235, 200, 285, 25);
+		
+		final Label lblPic = new Label(grpDetails, SWT.NONE);
+		lblPic.setBounds(10, 20, 200, 200);
+		
 		Group grpSearch = new Group(shlTvTraveler, SWT.NONE);
 		FormData fd_grpSearch = new FormData();
 		fd_grpSearch.bottom = new FormAttachment(0, 230);
@@ -143,8 +186,76 @@ public class MainDisplay {
 		btnRadioUsername.setText("Username");
 		
 		final List list = new List(grpSearch, SWT.BORDER | SWT.V_SCROLL);
+		list.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				int index = list.getSelectionIndex();
+				String sql = "";
+				
+				// Need to clean the labels...
+				lblDetails1.setText("");
+				lblDetails2.setText("");
+				lblDetails3.setText("");
+				lblDetails4.setText("");
+				lblDetails5.setText("");
+				lblDetails6.setText("");
+				lblPic.setImage(null);
+				
+				String currentSelection = list.getItem(index);				
+				if(currentSelection == null || currentSelection.isEmpty())
+					return;
+				
+				int id = -1;
+				
+				String[] split = currentSelection.split("ID:");
+				if(split.length != 2)
+					return;
+				
+				id = Integer.parseInt(split[1]);
+				
+				if(currentSearch.equals("TV"))
+				{
+					ResultSet rs;
+					sql = "SELECT * FROM Media, TV WHERE Media.media_id = TV.media_id AND Media.media_id = "+id;
+					rs = Main.performQuery(sql);
+								
+					try
+					{
+						if(rs == null || !rs.next())
+						{
+							System.out.println("Empty resultset");
+							return;
+						}
+						
+						String address = rs.getString("image");
+						
+						String name = rs.getString("name");
+						String director = rs.getString("directors");
+						Date first = rs.getDate("first_episode");
+						Date last = rs.getDate("last_episode");
+						int numSeasons = rs.getInt("num_seasons");
+						int numEpisodes = rs.getInt("num_episodes");
+						
+						URL url = new URL("http://img.freebase.com/api/trans/image_thumb"+address+"?maxheight=200&mode=fit&maxwidth=150");
+						Image img = new Image(display, url.openStream());						
+						lblPic.setImage(img);
+						lblDetails1.setText("Name: "+name);
+						lblDetails2.setText("Director(s): "+director);
+						lblDetails3.setText("First Episode: "+first.toString());
+						lblDetails4.setText("Last Episode: "+last.toString());
+						lblDetails5.setText("Number of Seasons: "+String.valueOf(numSeasons));
+						lblDetails6.setText("Number of Episodes: "+String.valueOf(numEpisodes));
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+						
+				}
+			}
+		});
 		list.setBounds(185, 72, 365, 148);
 		
+
 		
 		Button btnSearch = new Button(grpSearch, SWT.NONE);
 		btnSearch.addSelectionListener(new SelectionAdapter() {
@@ -266,43 +377,6 @@ public class MainDisplay {
 		});
 		btnSearch.setBounds(441, 26, 109, 30);
 		btnSearch.setText("Search");
-		
-		Group grpDetails = new Group(shlTvTraveler, SWT.NONE);
-		FormData fd_grpDetails = new FormData();
-		fd_grpDetails.bottom = new FormAttachment(0, 230);
-		fd_grpDetails.right = new FormAttachment(0, 1122);
-		fd_grpDetails.top = new FormAttachment(0);
-		fd_grpDetails.left = new FormAttachment(0, 576);
-		grpDetails.setLayoutData(fd_grpDetails);
-		grpDetails.setText("Details");
-		
-		Label lblDetails1 = new Label(grpDetails, SWT.BORDER);
-		lblDetails1.setBounds(10, 25, 247, 20);
-		
-		Label lblDetails2 = new Label(grpDetails, SWT.BORDER);
-		lblDetails2.setBounds(273, 25, 247, 20);
-		
-		Label lblDetails3 = new Label(grpDetails, SWT.BORDER);
-		lblDetails3.setBounds(10, 64, 247, 20);
-		
-		Label lblDetails4 = new Label(grpDetails, SWT.BORDER);
-		lblDetails4.setBounds(273, 64, 247, 20);
-		
-		Label lblDetails5 = new Label(grpDetails, SWT.BORDER);
-		lblDetails5.setBounds(10, 110, 247, 20);
-		
-		Label lblDetails6 = new Label(grpDetails, SWT.BORDER);
-		lblDetails6.setBounds(273, 110, 247, 20);
-		
-		Label lblDetails7 = new Label(grpDetails, SWT.BORDER);
-		lblDetails7.setBounds(10, 158, 247, 20);
-		
-		Label lblDetails8 = new Label(grpDetails, SWT.BORDER);
-		lblDetails8.setBounds(273, 158, 247, 20);
-		
-		Label lblDetails9 = new Label(grpDetails, SWT.BORDER);
-		lblDetails9.setBounds(10, 200, 510, 20);
-		
 		
 		FormData fd_composite = new FormData();
 		fd_composite.bottom = new FormAttachment(0, 64);
