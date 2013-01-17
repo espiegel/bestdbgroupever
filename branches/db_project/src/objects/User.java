a4e97636e90800994179904153a7183d2668fa30
eidanspiegel@gmail.com <eidanspiegel@gmail.com@0db8492b-292b-fea8-5af4-ef6f3a9017e6>
2013-01-17 12:58:21 +0000

    Added a retrieveById function to User.java

--- branches/db_project/src/objects/User.java
+++ branches/db_project/src/objects/User.java
@@ -2,6 +2,54 @@ package objects;
 
 public class User {
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + badges;
+		result = prime * result + downvotes;
+		result = prime * result + id;
+		result = prime * result + (isAdmin ? 1231 : 1237);
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result + upvotes;
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (badges != other.badges)
+			return false;
+		if (downvotes != other.downvotes)
+			return false;
+		if (id != other.id)
+			return false;
+		if (isAdmin != other.isAdmin)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (upvotes != other.upvotes)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
 	private String username;
 	private String password;
 	private int upvotes;
@@ -20,6 +68,7 @@ public class User {
 		isAdmin = false;
 		id = -1;
 	}
+	
 	public String getUsername() {
 		return username;
 	}
