bc90dbd902deeac5de37de4bd96119e09213084a
maayankil@gmail.com <maayankil@gmail.com@0db8492b-292b-fea8-5af4-ef6f3a9017e6>
2013-01-20 20:24:43 +0000

    DoubleClick now searches from actors list

---
+++ src/db/MediaByActorIDRetriever.java
@@ -0,0 +1,47 @@
+package db;
+
+import java.sql.ResultSet;
+
+import objects.Media;
+
+public class MediaByActorIDRetriever extends
+		RetrieverBase<Media> {
+
+	private final String[] default_fields = {"actor_id" };
+	private final String[] search_fields = { "name" };
+
+	@Override
+	protected String getTableNames() {
+		return "Media, ActorsInMedia";
+	}
+
+	@Override
+	protected String getJoinLine() {
+		return "ActorsInMedia.media_id = Media.media_id";
+	}
+
+	@Override
+	protected String[] getDefaultFields() {
+		return default_fields;
+	}
+
+	@Override
+	protected String[] getFieldForGeneralSearch() {
+		return search_fields;
+	}
+
+	@Override
+	protected Media makeObject(ResultSet result_set) {
+		try {
+			return fillObjectByFields(result_set, new Media());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public java.util.List<Media> searchByID(int id) {
+		return retrieve("actor_id = " + id);
+	}
+
+}
